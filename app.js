afb832d1833d3f4a0a2bb082529d2a64ae950ebe
vera <verapoles1@mail.ru>
2021-05-20 10:08:54 +0200

    refactor: add port

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
-var game = require('./controllers/gamecontroller')
+var game = require('./controllers/gamecontroller');
+//var bcrypt = require('bcryptjs');
 
 
 db.sync();
-app.use(require('body-parser'));
+app.use(express.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+app.listen(4000, function () {
     console.log("App is listening on 4000");
+    //console.log(bcrypt.hashSync("123", 10));
 })
